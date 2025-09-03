b47c6b99637faff9dbcd0386201765b2d08f6e95
Maksim Iliev <maksim.iliev@icloud.com>
2025-09-03 17:08:50 +0300

    fix vite base for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '',
+    base: '/EventPlanner/',
     build: {
         outDir: 'dist',
         sourcemap: true
     },
 })
+
